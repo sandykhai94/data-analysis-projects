ad93c50af72d37d597c2dc32c511e138ce2e890d
Sandy Khai <sandykhai94@gmail.com>
2025-10-09 17:07:16 -0500

    finished books table questions

--- sql-part-1/exercises/new-file.sql
+++ sql-part-1/exercises/new-file.sql
@@ -1,24 +1,29 @@
 -- Question One
-SELECT COUNT(1000)
-FROM [BooksDB]
+SELECT TOP 1000 *
+FROM [dbo].[books]
 
 -- QUESTION TWO
-SELECT COUNT(10000)
--- from BooksDB
-WHERE title
+SELECT COUNT(Title)
+FROM [dbo].[books]
 
 -- QUESTION THREE
 SELECT COUNT(*)
-FROM [BooksDB]
-WHERE PublicationYear < 1800
+FROM [dbo].[books]
+WHERE original_publication_year < 1800
 
 -- QUESTION FOUR
-SELECT DISTINCT Author
-FROM [BooksDB]
+SELECT DISTINCT authors
+FROM [dbo].[books]
+
 
 -- QUESTION FIVE
-SELECT BOOKS_COUNT(*)
-from [BooksDB]
-WHERE "eng" OR "eng-"
+SELECT books_count
+from [dbo].[books]
+WHERE language_code = 'eng'
+OR language_code = 'en-'
 
 -- QUESTION SIX
+SELECT original_title
+FROM [dbo].[books]
+WHERE original_publication_year
+BETWEEN 1914 AND 1921
