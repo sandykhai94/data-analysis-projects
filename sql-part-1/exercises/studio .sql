45b7d1707b199627d69a85ad25991ec6b4e16726
Sandy Khai <sandykhai94@gmail.com>
2025-10-09 23:16:21 -0500

    SQL part 1 studio

--- sql-part-1/exercises/studio .sql
+++ sql-part-1/exercises/studio .sql
@@ -3,6 +3,7 @@ use BooksDB
 select top 100 book_id,title,average_rating as "rating"
 from dbo.books
 where average_rating > 4.5
+order by rating DESC
 --The Complete Calvin and Hobbies
 
 --QUESTION TWO
@@ -18,6 +19,45 @@ order by "count" DESC
 --596234
 
 --QUESTION FOUR 
-select top 1 
+USE BooksDB
+SELECT tag_id, SUM("count") AS total_sum
+FROM book_tags
+GROUP BY tag_id
+ORDER BY total_sum DESC
+--30574
 
 --QUESTION FIVE
+SELECT COUNT(original_title) FROM BooksDB.dbo.books
+WHERE original_publication_year < 2010
+AND original_publication_year >= 2000
+
+--QUESTION SIX
+SELECT COUNT(original_title) FROM BooksDB.dbo.books
+WHERE title LIKE '%Happy%'
+
+--QUESTION SEVEN
+use BooksDB
+select *
+from dbo.books
+where average_rating > 4.5
+AND authors = 'J.K. Rowling'
+ORDER BY authors ASC, average_rating ASC
+
+--QUESTION EIGHT
+use BooksDB
+select COUNT(authors)
+from dbo.books
+WHERE authors < 'Roll'
+AND authors > 'Rock'
+
+/* PART TWO */
+--QUESTION ONE
+/* I'd like to know how many distinct authors there are in the database. */
+
+SELECT COUNT(DISTINCT authors) AS distinct_number_of_authors
+FROM BooksDB.dbo.books
+
+--QUESTION TWO
+/* I'd like to know how many distinct books there are in the database. */
+SELECT COUNT(DISTINCT title) AS distinct_number_of_books
+FROM BooksDB.dbo.books
