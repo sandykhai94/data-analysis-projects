3e14c218e6ecc9dafc3ea0adf931bbb1ab9eaea0
Sandy Khai <sandykhai94@gmail.com>
2025-11-06 20:07:34 -0600

    string mod. studio

--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -2,14 +2,22 @@ my_string = "LaunchCode"
 
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
-
-
+new_string=my_string[3:] + my_string[:3]
+print(new_string)
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
-
+print(f"The original string is {my_string} and the modified string is {new_string}")
 
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+num_letters_relocated_str = input("3")
+num_letters_relocated = int(3)
+print(f"3 {num_letters_relocated}3")
+
 
 # c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if len(my_string) > 10:
+    print("Max of 10 characters")
+else:
+    print("Nothing")
---
+++ sql-part-5/studio/lc demo.py
@@ -0,0 +1,82 @@
+#loop by elements
+'''
+expenses = [106, 400, 333, 150, 240, 370]
+
+for expense in expenses:
+    print(f"Expense after tax: ${expense * 1.08:.2f}")
+'''
+#expenses = [106, 400, 333, 150, 240, 370]
+'''
+for i in range(len(expenses)):
+    print(f"Expense number {i}: original expense {expenses[i]}")
+'''
+'''
+for i in range(len(expenses)):
+    if i % 3 === 0:
+        expenses[i] += 1000 #pad expenses by 1000
+    print(f"updated expenses: {expenses}")
+'''
+'''
+for i, expense in enumerate(expenses):
+    print(f"Index {i}, Value {expense}")
+expenses[i]
+'''
+
+'''
+#tuples
+employee = ("ID-104", "Alex Chen", "Data Analyst")
+print(employee[1])
+
+employee_as_list = list(employee)
+employee_as_list[1] = "Alex Young"
+employee = tuple(employee_as_list)
+print(employee)
+'''
+
+#Dictonaries: does not to be in order, but it is important to label 
+
+# employee = {
+#     "id": "E102",
+#     "name": "Alex Chen",
+#     "role": "Data Analyst",
+#     "salary": 72000
+# }
+# print(employee["name"])
+# print(employee["role"])
+
+# employee["department"] = "Research"
+# employee ["salary"] = 82000
+# print(employee)
+
+# del employee["department"]
+# print(employee)
+
+# for key in employee: 
+#     print(key, "-->", employee[key])
+
+# for key, value in employee.items():
+#     print(key, "-->", value)
+
+employees = {
+    "E102": {
+        "name": "Alex Chen",
+        "role": "Data Analyst",
+        "salary": 82000,
+        "department": "research"
+    }, 
+    "E103": {
+        "name": "Jordan Smith",
+        "role": "Data Engineer",
+        "salary": 88000,
+        "department": "infrastructure"
+    }
+}
+
+print(employees["E102"]["salary"])
+
+employees["E102"]["salary"] = 100000
+print(employees["E102"])
+
+#add location to values
+employees["E103"]["location"] = "remote"
+print(employees)
